5a71bb825d16f5ce554d77b9582238990c6b0786
anikettikkal <tikkalaniket2019@gmail.com>
2025-12-24 22:41:48 +0530

    operators

--- tutorial.c
+++ tutorial.c
@@ -1,8 +1,34 @@
 #include<stdio.h>
 int main(){
     // type conversion
+    /*
     int a = (int)4.343;
 
-    printf("%d", a);
+    printf("%d \n", a);
+    */
+
+    // operator precedence
+    /*
+    int x= 9+3*20;
+    printf("%d \n", x);
+    */
+
+    //relational operators
+    /*
+    int a=10;
+    int b=10;
+    printf("%d\n", a==b);
+    printf("%d\n", 4 != 4);
+    printf("%d", 4==4);
+    */
+
+    //logical operators
+    /*
+    printf("%d\n", 4>3 && 5<2);
+    printf("%d\n", 4<3 || 5<3);
+    printf("%d\n", !((4>3) && (5<2)));
+    */
+
+    //bitwise operators
     return 0;
 }

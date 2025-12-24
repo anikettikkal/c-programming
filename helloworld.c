a6f32922b12d1f8fab7871ea9eb510b34bf7c970
anikettikkal <tikkalaniket2019@gmail.com>
2025-12-24 11:44:08 +0530

    simple syntax for c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
+int main(){
+    int age=25;
+    char name = 'A';
+    float pi = 3.14;
+    printf("my age is %d \n",age);
+    printf("my name is %c \n",name);
+    printf("value ofpi %f \n",pi);
 
-int main() {
-    printf("Hello World");
+    printf("Hello, World!\n");
+    printf("Hello, World!");
+
     return 0;
 }

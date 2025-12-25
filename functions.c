2aa6e3e6636e85411e909501566a983c7f0e59c0
anikettikkal <tikkalaniket2019@gmail.com>
2025-12-25 22:32:43 +0530

    function declaration,defination and call

---
+++ functions.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void greet();  // function declaration
+
+
+int main(){
+    greet(); // function call
+    return 0;
+}
+
+// function definition
+
+void greet(){
+    printf("Happy Learning C Programming\n");
+}

cb79dbec380e9d3620dd50e502383b7965724c9f
anikettikkal <tikkalaniket2019@gmail.com>
2025-12-26 10:23:00 +0530

    passing argument function

--- functions.c
+++ functions.c
@@ -1,14 +1,42 @@
-#include<stdio.h>
-void greet();  // function declaration
+#include <stdio.h>
 
+int sum(int a, int b);
+    // void greet();
+    // void goodbye();  // function declaration
 
-int main(){
-    greet(); // function call
+    /*
+    int main(){
+        greet();
+        goodbye(); // function call
+        return 0;
+    }
+
+    // function definition
+
+    void greet(){
+        printf("Happy Learning C Programming\n");
+    }
+
+    void goodbye(){
+        printf("Goodbye! See you next time.\n");
+    }
+    */
+
+    int main()
+{
+
+    int a, b;
+
+    printf("enter values of a: %d\n");
+    scanf("%d", &a);
+    printf("enter values of b: %d\n");
+    scanf("%d", &b);
+    int result = sum(a,b);
+    printf("The sum is: %d\n", result);
     return 0;
 }
 
-// function definition
-
-void greet(){
-    printf("Happy Learning C Programming\n");
+int sum(int a, int b)
+{
+    return a + b;
 }

d13c9a644ce4d2a98f255209dc9bd116a3c5b946
anikettikkal <tikkalaniket2019@gmail.com>
2025-12-26 10:59:18 +0530

    recursion function

--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int sum(int a, int b);
+// int sum(int a, int b);
     // void greet();
     // void goodbye();  // function declaration
 
@@ -22,6 +22,8 @@ int sum(int a, int b);
     }
     */
 
+    /*
+
     int main()
 {
 
@@ -39,4 +41,41 @@ int sum(int a, int b);
 int sum(int a, int b)
 {
     return a + b;
+}
+    */
+
+// library functions 
+/*
+#include <math.h>
+void square(int a);
+
+int main(){
+    int a;
+    printf("enter number %d\n");
+    scanf("%d", &a);
+    
+    square(a);
+    return 0;
+}
+
+void square(int a){
+    int result = pow(a,2);
+    printf("The square is: %d\n", result);
+}
+*/
+
+// recursive functions = a function that calls itself
+
+// print hello world five times using recursion
+void printHw(int n);
+int main(){
+    printHw(5);
+    return 0;
+}
+void printHw(int n){
+    if(n==0){
+        return;
+    }
+    printf("Hello World\n");
+    printHw(n-1);
 }
